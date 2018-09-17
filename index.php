cb1af8c9286574c51392d26ee7ce163b9269f597
jeremy hennebique <jimimi@hotmail.fr>
2018-09-17 10:06:08 +0200

    solutionner gestion des items non existants

--- index.php
+++ index.php
@@ -10,22 +10,14 @@ $data = file_get_contents('https://eu.api.battle.net/wow/item/18803?locale=fr_FR
 $item = json_decode($data);*/
 require_once('class/Item.php');
 
-/*$itemId = '18803';
-$item = new Item($itemId);
 
-$itemName = $item->getItemName();
-echo $itemName;
-
-$itemDesc = $item->getDescription();
-echo $itemDesc;
-*/
-
-for($itemId = 18803; $itemId <= 18823; $itemId++){
+for($itemId = 1175; $itemId <= 1180; $itemId++){
 
   $item = new Item($itemId);
 
   $itemName = $item->getItemName();
   $itemClass =$item->getItemClass();
+
 
   echo $itemId.' '.$itemName.' '.$itemClass.'</br>';
 
